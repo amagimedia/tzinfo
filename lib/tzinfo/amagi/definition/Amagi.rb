8011512fd72a47342e2efcc927586b7cb3e08274
samarth23b <samarth.benawat@amagi.com>
2024-02-01 13:56:39 +0530

    add constants separately

--- lib/tzinfo/amagi/definition/Amagi.rb
+++ lib/tzinfo/amagi/definition/Amagi.rb
@@ -2,12 +2,17 @@ module TZInfo
   module Amagi
     module Definition
       module Amagi
+
+        DEFAULT_SWITCH_OVER_EPOCH = "08:30:00"
+        DEFAULT_ADST_OFFSET = 23400
+        DEFAULT_INITIAL_OFFSET = 19800
+
         include TimezoneDefinition
         d = Date.current
         d = Date.parse(ENV["AMAGI_DST_DATE"]) if ENV["AMAGI_DST_DATE"]
-        switch_over_epoch = ENV["AMAGI_DST_SWITCH_TIME"] ? Time.find_zone("UTC").parse(d.strftime("%Y-%m-%d") + " " + ENV["AMAGI_DST_SWITCH_TIME"] ).to_i : Time.find_zone("UTC").parse(d.strftime("%Y-%m-%d") + " " + "08:30:00").to_i
-        adst_offset = ENV["AMAGI_DST_SWITCH_OFFSET"] ? (ENV["AMAGI_DST_SWITCH_OFFSET"]).to_i : 23400
-        initial_offset = ENV["AMAGI_DST_INITIAL_OFFSET"] ? (ENV["AMAGI_DST_INITIAL_OFFSET"]).to_i : 19800
+        switch_over_epoch = ENV["AMAGI_DST_SWITCH_TIME"] ? Time.find_zone("UTC").parse(d.strftime("%Y-%m-%d") + " " + ENV["AMAGI_DST_SWITCH_TIME"] ).to_i : Time.find_zone("UTC").parse(d.strftime("%Y-%m-%d") + " " + DEFAULT_SWITCH_OVER_EPOCH).to_i
+        adst_offset = ENV["AMAGI_DST_SWITCH_OFFSET"] ? (ENV["AMAGI_DST_SWITCH_OFFSET"]).to_i : DEFAULT_ADST_OFFSET
+        initial_offset = ENV["AMAGI_DST_INITIAL_OFFSET"] ? (ENV["AMAGI_DST_INITIAL_OFFSET"]).to_i : DEFAULT_INITIAL_OFFSET
         timezone 'Amagi' do |tz|
           tz.offset :o0, initial_offset, 0, :AST
           tz.offset :o1, adst_offset, 0, :ADST
